04fe9fac94b40386bc423de886e6c64459b3c6d8
MeghanaBait <meghanawork777@gmail.com>
2023-08-12 18:33:47 +0530

    Adding Getters

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,14 @@ public class Boat implements WaterVehicle{
     String name;
     int capacity;
 
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
     public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
